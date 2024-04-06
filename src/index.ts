ec7e63c3ccfa0be92ca0a62519078cfd02d2dd16
Andres Melara <andres.melara.t@gmail.com>
2024-04-06 04:18:01 -0400

    Filter products by a specific category

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,16 @@ import catalog from "./data.json" assert { type: "json" };
 const productNames = catalog.map((item) => item.name);
 
 console.log(productNames);
+
+//  1. Filter products by a specific category.
+
+const category1 = "Accessories";
+const category2 = "Clothing";
+
+const filteredProducts = catalog.filter((item) => {
+  if (item.category === category1) {
+    return item;
+  }
+});
+
+console.log(filteredProducts);
