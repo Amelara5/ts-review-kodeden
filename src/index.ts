b6423e71817d5f4044e5869ee6b7474f7d91976d
Andres Melara <andres.melara.t@gmail.com>
2024-04-06 04:42:02 -0400

    Reduce the catalog to the total price of all products

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ console.log(productNames);
 //  1. Filter products by a specific category.
 
 const category1 = "Accessories";
-const category2 = "Clothing";
+// const category2 = "Clothing";
 
 const filteredProducts = catalog.filter((item) => {
   if (item.category === category1) {
@@ -25,3 +25,15 @@ const shortCatalog = catalog.map((item) => {
 
 console.log(shortCatalog);
 
+//  3. Reduce the catalog to the total price of all products
+
+const totalPrice = catalog.reduce(
+  (accumulator, currentItem) => accumulator + currentItem.price,
+  0,
+);
+
+console.log(totalPrice);
+
+//  4. Write a function that takes in the catalog and a category, filters by category and then returns the total price of all products in that category.
+
+const 
